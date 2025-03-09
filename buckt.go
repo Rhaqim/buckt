d17f3de46b19e4f47a8b93b7c5617e6dc511b151
Rhaqim <anusiemj@gmail.com>
2025-03-09 19:40:54 +0100

    docs for rename, folder methods first before file methods

--- buckt.go
+++ buckt.go
@@ -117,59 +117,7 @@ func (b *Buckt) Close() {
 	b.db.Close()
 }
 
-// UploadFile uploads a file to the specified user's bucket.
-//
-// Parameters:
-//   - user_id: The ID of the user who owns the bucket.
-//   - parent_id: The ID of the parent directory where the file will be uploaded.
-//   - file_name: The name of the file to be uploaded.
-//   - content_type: The MIME type of the file.
-//   - file_data: The byte slice containing the file data.
-//
-// Returns:
-//   - string: The ID of the newly created file.
-//   - error: An error if the file upload fails, otherwise nil.
-func (b *Buckt) UploadFile(user_id string, parent_id string, file_name string, content_type string, file_data []byte) (string, error) {
-	return b.fileService.CreateFile(user_id, parent_id, file_name, content_type, file_data)
-}
-
-// GetFile retrieves a file based on the provided file ID.
-// It returns the file data and an error, if any occurred during the retrieval process.
-//
-// Parameters:
-//   - file_id: A string representing the unique identifier of the file to be retrieved.
-//
-// Returns:
-//   - *model.FileModel: The file data.
-//   - error: An error object if an error occurred, otherwise nil.
-func (b *Buckt) GetFile(file_id string) (*model.FileModel, error) {
-	return b.fileService.GetFile(file_id)
-}
-
-// MoveFile moves a file to a new parent directory.
-//
-// Parameters:
-//   - file_id: The ID of the file to be updated.
-//   - new_parent_id: The new parent directory for the file.
-//
-// Returns:
-//   - error: An error if the update operation fails, otherwise nil.
-func (b *Buckt) MoveFile(file_id string, new_parent_id string) error {
-	return b.fileService.MoveFile(file_id, new_parent_id)
-}
-
-// DeleteFile deletes a file associated with the given user ID and file ID.
-// It returns an error if the deletion fails.
-//
-// Parameters:
-//   - file_id: The ID of the file to be deleted.
-//
-// Returns:
-//   - error: An error if the file deletion fails, otherwise nil.
-func (b *Buckt) DeleteFile(file_id string) error {
-	_, err := b.fileService.DeleteFile(file_id)
-	return err
-}
+/* Folder Methods */
 
 // NewFolder creates a new folder for a user within a specified parent folder.
 // It takes the following parameters:
@@ -194,20 +142,6 @@ func (b *Buckt) NewFolder(user_id string, parent_id string, folder_name string, 
 //	error - An error if the folder could not be retrieved.
 func (b *Buckt) ListFolders(folder_id string) ([]model.FolderModel, error) {
 	return b.folderService.GetFolders(folder_id)
-}
-
-// ListFiles retrieves a list of files for a given folder.
-//
-// Parameters:
-//
-//	folder_id - The ID of the folder to retrieve.
-//
-// Returns:
-//
-//	[]model.FileModel - A list of files.
-//	error - An error if the folder could not be retrieved.
-func (b *Buckt) ListFiles(folder_id string) ([]model.FileModel, error) {
-	return b.fileService.GetFiles(folder_id)
 }
 
 // GetFolderWithContent retrieves a folder and its content.
@@ -240,7 +174,17 @@ func (b *Buckt) MoveFolder(user_id, folder_id string, new_parent_id string) erro
 	return b.folderService.MoveFolder(folder_id, new_parent_id)
 }
 
-// RenameFolder implements Buckt.
+// RenameFolder renames a folder.
+//
+// Parameters:
+//
+//	user_id: The ID of the user performing the operation.
+//	folder_id: The ID of the folder to be renamed.
+//	new_name: The new name for the folder.
+//
+// Returns:
+//
+//	error: An error if the operation fails, otherwise nil.
 func (b *Buckt) RenameFolder(user_id, folder_id string, new_name string) error {
 	return b.folderService.RenameFolder(user_id, folder_id, new_name)
 }
@@ -248,4 +192,74 @@ func (b *Buckt) RenameFolder(user_id, folder_id string, new_name string) error {
 // DeleteFolder implements Buckt.
 func (b *Buckt) DeleteFolder(user_id, folder_id string) error {
 	panic("unimplemented")
+}
+
+/* File Methods */
+
+// UploadFile uploads a file to the specified user's bucket.
+//
+// Parameters:
+//   - user_id: The ID of the user who owns the bucket.
+//   - parent_id: The ID of the parent directory where the file will be uploaded.
+//   - file_name: The name of the file to be uploaded.
+//   - content_type: The MIME type of the file.
+//   - file_data: The byte slice containing the file data.
+//
+// Returns:
+//   - string: The ID of the newly created file.
+//   - error: An error if the file upload fails, otherwise nil.
+func (b *Buckt) UploadFile(user_id string, parent_id string, file_name string, content_type string, file_data []byte) (string, error) {
+	return b.fileService.CreateFile(user_id, parent_id, file_name, content_type, file_data)
+}
+
+// GetFile retrieves a file based on the provided file ID.
+// It returns the file data and an error, if any occurred during the retrieval process.
+//
+// Parameters:
+//   - file_id: A string representing the unique identifier of the file to be retrieved.
+//
+// Returns:
+//   - *model.FileModel: The file data.
+//   - error: An error object if an error occurred, otherwise nil.
+func (b *Buckt) GetFile(file_id string) (*model.FileModel, error) {
+	return b.fileService.GetFile(file_id)
+}
+
+// ListFiles retrieves a list of files for a given folder.
+//
+// Parameters:
+//
+//	folder_id - The ID of the folder to retrieve.
+//
+// Returns:
+//
+//	[]model.FileModel - A list of files.
+//	error - An error if the folder could not be retrieved.
+func (b *Buckt) ListFiles(folder_id string) ([]model.FileModel, error) {
+	return b.fileService.GetFiles(folder_id)
+}
+
+// MoveFile moves a file to a new parent directory.
+//
+// Parameters:
+//   - file_id: The ID of the file to be updated.
+//   - new_parent_id: The new parent directory for the file.
+//
+// Returns:
+//   - error: An error if the update operation fails, otherwise nil.
+func (b *Buckt) MoveFile(file_id string, new_parent_id string) error {
+	return b.fileService.MoveFile(file_id, new_parent_id)
+}
+
+// DeleteFile deletes a file associated with the given user ID and file ID.
+// It returns an error if the deletion fails.
+//
+// Parameters:
+//   - file_id: The ID of the file to be deleted.
+//
+// Returns:
+//   - error: An error if the file deletion fails, otherwise nil.
+func (b *Buckt) DeleteFile(file_id string) error {
+	_, err := b.fileService.DeleteFile(file_id)
+	return err
 }
