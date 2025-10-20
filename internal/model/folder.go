83d7f44e431978da9fad44c8b7e6f5bcf116cc93
Rhaqim <anusiemj@gmail.com>
2025-10-20 06:22:58 +0100

    changed the composite unique constraint on folders to include user id

--- internal/model/folder.go
+++ internal/model/folder.go
@@ -8,14 +8,14 @@ import (
 )
 
 type FolderModel struct {
-	ID          uuid.UUID      `gorm:"type:uuid;primaryKey" json:"id"`                                 // Unique identifier for the file
-	UserID      string         `gorm:"not null" json:"user_id"`                                        // ID of the user who owns the bucket
-	ParentID    *uuid.UUID     `gorm:"type:uuid;uniqueIndex:idx_folder_parent_name" json:"parent_id"`  // Foreign key to FolderModel
-	Name        string         `gorm:"not null;uniqueIndex:idx_folder_parent_name" json:"name"`        // Folder name
-	Description string         `gorm:"type:text" json:"description"`                                   // Optional description of the bucket
-	Path        string         `gorm:"not null;unique" json:"path"`                                    // File path
-	Folders     []FolderModel  `gorm:"foreignKey:ParentID;constraint:OnDelete:CASCADE" json:"folders"` // Establish one-to-many relationship with FolderModel
-	Files       []FileModel    `gorm:"foreignKey:ParentID;constraint:OnDelete:CASCADE" json:"files"`   // Establish one-to-many relationship with FileModel
+	ID          uuid.UUID      `gorm:"type:uuid;primaryKey" json:"id"`                                     // Unique identifier for the file
+	UserID      string         `gorm:"not null;uniqueIndex:idx_folder_user_parent_name" json:"user_id"`    // ID of the user who owns the bucket
+	ParentID    *uuid.UUID     `gorm:"type:uuid;uniqueIndex:idx_folder_user_parent_name" json:"parent_id"` // Foreign key to FolderModel
+	Name        string         `gorm:"not null;uniqueIndex:idx_folder_user_parent_name" json:"name"`       // Folder name
+	Description string         `gorm:"type:text" json:"description"`                                       // Optional description of the bucket
+	Path        string         `gorm:"not null;unique" json:"path"`                                        // File path
+	Folders     []FolderModel  `gorm:"foreignKey:ParentID;constraint:OnDelete:CASCADE" json:"folders"`     // Establish one-to-many relationship with FolderModel
+	Files       []FileModel    `gorm:"foreignKey:ParentID;constraint:OnDelete:CASCADE" json:"files"`       // Establish one-to-many relationship with FileModel
 	CreatedAt   time.Time      `json:"created_at"`
 	UpdatedAt   time.Time      `json:"updated_at"`
 	DeletedAt   gorm.DeletedAt `gorm:"index" json:"deleted_at"`
